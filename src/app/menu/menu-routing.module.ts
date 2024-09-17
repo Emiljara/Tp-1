97a6952236e3bc86a5a4bd9ebcd7ec3a10ddb2f0
Emiljara <emilianocba1@gmail.com>
2024-09-17 19:46:17 -0300

    modified:   src/app/menu/menu-routing.module.ts

--- src/app/menu/menu-routing.module.ts
+++ src/app/menu/menu-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { MenuPage } from './menu.page';
+import { authGuard } from '../guards/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
-    component: MenuPage
+    component: MenuPage,
+    canActivate: [authGuard]
   }
 ];
 
